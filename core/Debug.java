e27b1fd6689dec293ebe2125c540423be81091d3
Andrew Keesler <ankeesler1@gmail.com>
2015-01-11 18:53:51 -0500

    Add a debug helper class.

---
+++ core/Debug.java
@@ -0,0 +1,12 @@
+package core;
+
+/** An internal debug class for the meh project.
+ *
+ * @author Andrew Keesler
+ * @date January 11, 2015
+ */
+public class Debug {
+  
+  public static void println(String stuff) { System.out.println(stuff); }
+
+}
