b7f684108e86a73bd1a531a6c818a7d1100a9347
Andrew Keesler <ankeesler1@gmail.com>
2015-01-12 18:41:29 -0500

    Add ability to turn on and off debug messages.

--- core/Debug.java
+++ core/Debug.java
@@ -7,6 +7,26 @@ package core;
  */
 public class Debug {
   
-  public static void println(String stuff) { System.out.println(stuff); }
+  /** An enum for which 'channel' or module the debug help is for. */
+  public enum DebugChannel {
+    DEFAULT(true), /* the always on debug channel */
+      
+    CORE(true),    /* the core debug channel */
+    VIEW(true),    /* the view debug channel */
+    GRAPH(true);   /* the graph debug channel */
+      
+    boolean isOn;
+    
+    DebugChannel(boolean isOn) { this.isOn = isOn; }
+  }
+  
+  public static void println(String stuff) {
+    println(DebugChannel.DEFAULT, stuff);
+  }
+  
+  public static void println(Debug.DebugChannel channel, String stuff) {
+    if (channel.isOn)
+      System.out.println(stuff);
+  }
 
 }
